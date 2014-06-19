fa4fbd46502cef8e7816391956eb9a50cd73baaa
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2014-06-18 19:47:53 -0500

    drive: add song list support
    Change-Id: Iacdda3f795cb8f9d9ad185a0478d262d9d63f148

--- drive/src/main/java/org/opensilk/music/plugin/drive/DriveLibraryService.java
+++ drive/src/main/java/org/opensilk/music/plugin/drive/DriveLibraryService.java
@@ -18,7 +18,6 @@
 package org.opensilk.music.plugin.drive;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.os.RemoteException;
 import android.text.TextUtils;
@@ -27,30 +26,30 @@ import com.google.android.gms.auth.GoogleAuthException;
 import com.google.api.services.drive.Drive;
 import com.google.api.services.drive.model.File;
 import com.google.api.services.drive.model.FileList;
-import com.google.api.services.drive.model.ParentReference;
 
 import org.opensilk.music.api.RemoteLibraryService;
 import org.opensilk.music.api.callback.AlbumQueryResult;
 import org.opensilk.music.api.callback.ArtistQueryResult;
 import org.opensilk.music.api.callback.FolderBrowseResult;
+import org.opensilk.music.api.callback.SongQueryResult;
 import org.opensilk.music.api.model.Folder;
 import org.opensilk.music.api.model.Song;
 import org.opensilk.music.plugin.drive.ui.LibraryChooserActivity;
 import org.opensilk.music.plugin.drive.util.DriveHelper;
+import org.opensilk.music.plugin.drive.util.Helpers;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
 
 import javax.inject.Inject;
 
 import dagger.ObjectGraph;
 import hugo.weaving.DebugLog;
-import timber.log.Timber;
 
 import static android.os.AsyncTask.THREAD_POOL_EXECUTOR;
 import static org.opensilk.music.api.Api.Ability.BROWSE_FOLDERS;
+import static org.opensilk.music.api.Api.Ability.QUERY_SONGS;
 
 /**
  * Created by drew on 6/13/14.
@@ -84,7 +83,7 @@ public class DriveLibraryService extends RemoteLibraryService {
 
     @Override
     protected int getCapabilities() throws RemoteException {
-        return BROWSE_FOLDERS;
+        return BROWSE_FOLDERS|QUERY_SONGS;
     }
 
     @Override
@@ -113,10 +112,9 @@ public class DriveLibraryService extends RemoteLibraryService {
             public void run() {
                 try {
                     Drive.Files.List req = mDrive.drive().files().list()
-//                            .setQ(fID + " in parents and trashed=false")
                             .setQ(fID + " in parents and trashed=false and (mimeType = 'application/vnd.google-apps.folder'"
                                     + " or mimeType contains 'audio' or mimeType = 'application/ogg')")
-                            .setFields("items/id,items/mimeType,items/parents,items/title,items/downloadUrl")
+                            .setFields(Helpers.FIELDS)
                             .setMaxResults(maxResults);
                     if (!TextUtils.isEmpty(paginationToken)) {
                         req.setPageToken(paginationToken);
@@ -125,24 +123,15 @@ public class DriveLibraryService extends RemoteLibraryService {
                     List<File> files = resp.getItems();
                     List<Folder> folders = new ArrayList<>();
                     List<Song> songs = new ArrayList<>();
+                    final String authToken = mDrive.getAuthToken();
                     for (File f : files) {
                         final String mime = f.getMimeType();
                         if (TextUtils.equals("application/vnd.google-apps.folder", mime)) {
-                            Timber.d(f.toPrettyString());
-                            final String id = f.getId();
-                            final String title = f.getTitle();
-                            List<ParentReference> parents = f.getParents();
-                            final String parentId = parents.size() > 0 ? parents.get(0).getId() : null;
-                            folders.add(new Folder(id, title, parentId));
+                            Folder folder = Helpers.buildFolder(f);
+                            folders.add(folder);
                         } else if (mime.contains("audio") || TextUtils.equals(mime, "application/ogg")) {
-                            Timber.d(f.toPrettyString());
-                            final String id = f.getId();
-                            final String title = f.getTitle();
-                            final String album = null;
-                            final String artist = null;
-                            final int duration = 0;
-                            final Uri data = buildDownloadUri(f.getDownloadUrl(), mDrive.getAuthToken());
-                            songs.add(new Song(id, title, album, artist, duration, data, Uri.EMPTY));
+                            Song song = Helpers.buildSong(f, authToken);
+                            songs.add(song);
                         }
                     }
                     final Bundle b;
@@ -180,6 +169,62 @@ public class DriveLibraryService extends RemoteLibraryService {
 
     }
 
+    @Override
+    protected void querySongs(String libraryIdentity, final int maxResults, Bundle paginationBundle, final SongQueryResult cb) throws RemoteException {
+        mDrive.setAccountName(libraryIdentity);
+        final String paginationToken;
+        if (paginationBundle != null) {
+            paginationToken = paginationBundle.getString("token");
+        } else {
+            paginationToken = null;
+        }
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Drive.Files.List req = mDrive.drive().files().list()
+                            .setQ("trashed=false and (mimeType contains 'audio' or mimeType = 'application/ogg')")
+                            .setFields(Helpers.FIELDS)
+                            .setMaxResults(maxResults);
+                    if (!TextUtils.isEmpty(paginationToken)) {
+                        req.setPageToken(paginationToken);
+                    }
+                    FileList resp = req.execute();
+                    List<File> files = resp.getItems();
+                    List<Song> songs = new ArrayList<>();
+                    final String authToken = mDrive.getAuthToken();
+                    for (File f : files) {
+                        final String mime = f.getMimeType();
+                        if (mime.contains("audio") || TextUtils.equals(mime, "application/ogg")) {
+                            Song song = Helpers.buildSong(f, authToken);
+                            songs.add(song);
+                        }
+                    }
+                    final Bundle b;
+                    if (!TextUtils.isEmpty(resp.getNextPageToken())) {
+                        b = new Bundle(1);
+                        b.putString("token", resp.getNextPageToken());
+                    } else {
+                        b = null;
+                    }
+                    try {
+                        cb.success(songs, b);
+                    } catch (RemoteException e) {
+                        e.printStackTrace();
+                    }
+                } catch (IOException|GoogleAuthException e) {
+                    e.printStackTrace();
+                    try {
+                        cb.failure(""+e.getMessage());
+                    } catch (RemoteException e1) {
+                        e1.printStackTrace();
+                    }
+                }
+            }
+        };
+        THREAD_POOL_EXECUTOR.execute(r);
+    }
+
     protected void downloadSong(String sourceIdentity, final Song song) {
 //        if (!TextUtils.equals(mAccountCred.getSelectedAccountName(), sourceIdentity)) {
 //            mAccountCred.setSelectedAccountName(sourceIdentity);
@@ -207,10 +252,6 @@ public class DriveLibraryService extends RemoteLibraryService {
 //            }
 //        });
 //        t.start();
-    }
-
-    private static Uri buildDownloadUri(String url, String authToken) {
-        return Uri.parse(String.format(Locale.US, "%s&access_token=%s", url, authToken));
     }
 
 }
---
+++ drive/src/main/java/org/opensilk/music/plugin/drive/util/Helpers.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2014 OpenSilk Productions LLC
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.opensilk.music.plugin.drive.util;
+
+import android.net.Uri;
+
+import com.google.api.services.drive.model.File;
+import com.google.api.services.drive.model.ParentReference;
+
+import org.opensilk.music.api.model.Folder;
+import org.opensilk.music.api.model.Song;
+
+import java.util.List;
+import java.util.Locale;
+
+/**
+ * Created by drew on 6/18/14.
+ */
+public class Helpers {
+
+    public static final String FIELDS = "items/id,items/mimeType,items/parents,items/title,items/downloadUrl";
+
+    public static Folder buildFolder(File f) {
+        final String id = f.getId();
+        final String title = f.getTitle();
+        List<ParentReference> parents = f.getParents();
+        final String parentId = parents.size() > 0 ? parents.get(0).getId() : null;
+        return new Folder(id, title, parentId);
+    }
+
+    public static Song buildSong(File f, String authToken) {
+        final String id = f.getId();
+        final String title = f.getTitle();
+        final String album = null;
+        final String artist = null;
+        final int duration = 0;
+        final Uri data = buildDownloadUri(f.getDownloadUrl(), authToken);
+        return new Song(id, title, album, artist, duration, data, Uri.EMPTY);
+    }
+
+    public static Uri buildDownloadUri(String url, String authToken) {
+        return Uri.parse(buildDownloadUriString(url, authToken));
+    }
+
+    public static String buildDownloadUriString(String url, String authToken) {
+        return String.format(Locale.US, "%s&access_token=%s", url, authToken);
+    }
+}
